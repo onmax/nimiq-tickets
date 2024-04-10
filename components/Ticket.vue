192415b4302973b8efc517d7cb0662c9ecbe2edb
Maxi <maximogarciamtnez@gmail.com>
2024-04-10 18:43:50 +0200

    console.log

--- components/Ticket.vue
+++ components/Ticket.vue
@@ -2,7 +2,7 @@
 const { loggedIn, user, clear } = useUserSession()
 
 const { data: ticket } = await useFetch('/api/tickets', { query: { username: user.value?.username }})
-console.log(ticket)
+console.log(JSON.stringify(ticket))
 if(!ticket.value) {
   console.log(`no ticket found for ${user.value?.username}`)
   const ticketData = { username: user.value?.username, name: user.value?.name, avatar: user.value?.avatar }
